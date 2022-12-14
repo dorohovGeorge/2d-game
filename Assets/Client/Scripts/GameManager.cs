d3a3f7e0dd942ff34425d7ed35cf2f0608b58714
Daniil Shilov <shilov.da2001@gmail.com>
2022-12-14 20:19:32 +0300

    Fix timer

--- Assets/Client/Scripts/GameManager.cs
+++ Assets/Client/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -12,14 +13,42 @@ public class GameManager : MonoBehaviour
     
     [SerializeField] public int roomsSpawnLimit;
     public int roomsSpawned;
+    
+    public bool pause;
 
     private void Start()
     {
+        pause = false;
         DisplayCoins();
     }
 
     public void DisplayCoins()
     {
-        coinsText.text = DataHolder.coins.GetCoins().ToString();
+        if (!coinsText.IsUnityNull())
+        {
+            coinsText.text = DataHolder.coins.GetCoins().ToString();
+        }
+    }
+    
+    public static void Begin()
+    {
+        Timer.Begin();
+    }
+    
+    public void Pause()
+    {
+        pause = true;
+        Timer.Pause();
+    }
+    
+    public void Unpause()
+    {
+        pause = false;
+        Timer.Unpause();
+    }
+
+    public void End()
+    {
+        Timer.End(pause);
     }
 }
